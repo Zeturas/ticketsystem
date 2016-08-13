b9b7637540b5c1a061b623aca58849c27e3ff301
Laurent Fey <luxlink2@hotmail.com>
2016-08-13 16:18:49 +0200

    first

---
+++ App_Plugins/theTicketSystem/backoffice/client.resource.js
@@ -0,0 +1,17 @@
+﻿angular.module("umbraco.resources")
+.factory("clientResource", function ($http) {
+    return {
+        getAll: function() {
+            return $http.get("backoffice/theTicketSystem/ClientApi/GetAll");
+        },
+        getById: function (id) {
+            return $http.get("backoffice/theTicketSystem/ClientApi/GetById?id=" + id);
+        },
+        save: function (client) {
+            return $http.post("backoffice/theTicketSystem/ClientApi/PostSave", angular.toJson(client));
+        },
+        deleteById: function (id) {
+            return $http.delete("backoffice/theTicketSystem/ClientApi/DeleteById?id=" + id);
+        }
+    }
+});
---
+++ app_code/TheTicketSystem/RegisterEvents.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Core;
+using Umbraco.Core.Persistence;
+
+public class RegisterEvents : ApplicationEventHandler
+{
+    protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+    {
+        var db = applicationContext.DatabaseContext.Database;
+
+        // Create client table if not exists
+        if (!db.TableExist("Client"))
+        {
+            db.CreateTable<Client>(true);
+        }
+
+        // Create ticket table if not exists
+        if (!db.TableExist("Ticket"))
+        {
+            db.CreateTable<Ticket>(true);
+        }
+
+        // Create ticketText table if not exists
+        if (!db.TableExist("TicketText"))
+        {
+            db.CreateTable<TicketText>(true);
+        }
+    }
+}
